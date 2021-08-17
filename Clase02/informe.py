bdaefd931cdef29d85100abb399f1a8a388eacac
VMenga <valentin.mengascini@gmail.com>
2021-08-17 19:48:54 -0300

    fix camion_commandline.py

--- Clase02/informe.py
+++ Clase02/informe.py
@@ -4,17 +4,21 @@ import csv
 import sys
 
 def leer_camion(archivo_camion):
-    costo = 0
+    costo_camion = 0
     f = csv.reader(open(archivo_camion, 'rt'), delimiter=',')
     header = next(f)
-    for row in f: 
-        costo += int(row[1]) * float(row[2])
-    return costo
+    for row in f:
+        costo_camion += int(row[1]) * float(row[2])
+    return costo_camion
 
 
 def leer_precios(archivo_precios):
     precios = csv.reader(open(archivo_precios, 'rt'), delimiter=',')
-    precio_fruta = 0
+    venta_fruta = 0
     for linea in precios:
-        if fruta in linea:
-            precio_fruta = linea[1]
+        venta_fruta = linea[1]
+    pass
+
+
+for s in costo_camion:
+    total += s['cajones']*s['precio']
