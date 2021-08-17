b693665cbbb58c0129fb9dd8725cdab93ed78162
VMenga <valentin.mengascini@gmail.com>
2021-08-17 19:38:16 -0300

    fix camion_commandline.py

--- Clase02/camion_commandline.py
+++ Clase02/camion_commandline.py
@@ -10,11 +10,11 @@ def costo_camion(nombre_archivo):
     for row in f:
         costo += int(row[1]) * float(row[2])
     return costo
-    
+
 if len(sys.argv) == 2:
     nombre_archivo = sys.argv[1]
 else:
-    nombre_archivo = 'camion.csv'
+    nombre_archivo = '../Data/camion.csv'
 
 costo = costo_camion(nombre_archivo)
 print('Costo total:', costo)
