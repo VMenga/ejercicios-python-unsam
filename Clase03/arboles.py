e2fbe3464d3989ac459a6bca65b310269510f0fc
VMenga <valentin.mengascini@gmail.com>
2021-08-25 13:00:58 -0300

    modded arboles.py

--- Clase03/arboles.py
+++ Clase03/arboles.py
@@ -1,35 +1,48 @@
 #arboles.py
 #Por Valentin Mengascini
 import csv
+import sys
+from pprint import pprint
 
-def leer_parque(nombre_archivo):
+def leer_parque(nombre_archivo, parque):
     arboles = []
     f = csv.reader(open(nombre_archivo, 'rt'), delimiter=',')
     encabezados = next(f)
     for n_fila, fila in enumerate(f, start=1):
         record = dict(zip(encabezados, fila))
-        try:
-            dicc = {}
-            dicc['long'] = float(record['long'])
-            dicc['lat'] = float(record['lat'])
-            dicc['id_arbol'] = int(record['id_arbol'])
-            dicc['altura_tot'] = int(record['altura_tot'])
-            dicc['diametro'] = int(record['diametro'])
-            dicc['inclinacio'] = int(record['inclinacio'])
-            dicc['id_especie'] = int(record['id_especie'])
-            dicc['nombre_com'] = str(record['nombre_com'])
-            dicc['nombre_cie'] = str(record['nombre_cie'])
-            dicc['tipo_folla'] =str(record['tipo_folla'])
-            dicc['espacio_ve'] =str(record['espacio_ve'])
-            dicc['ubicacion'] =str(record['ubicacion'])
-            dicc['nombre_fam'] =str(record['nombre_fam'])
-            dicc['nombre_gen'] =str(record['nombre_gen'])
-            dicc['origen'] = str(record['origen'])
-            dicc['coord_x'] = float(record['coord_x'])
-            dicc['coord_y'] = float(record['coord_y'])
-            arboles.append(dicc)
-        except ValueError:
-            print(f'Fila {n_fila}: No pude interpretar: {fila}')
+        if record['espacio_ve'] == parque:
+            try:
+                dicc = {}
+                dicc['long'] = float(record['long'])
+                dicc['lat'] = float(record['lat'])
+                dicc['id_arbol'] = int(record['id_arbol'])
+                dicc['altura_tot'] = int(record['altura_tot'])
+                dicc['diametro'] = int(record['diametro'])
+                dicc['inclinacio'] = int(record['inclinacio'])
+                dicc['id_especie'] = int(record['id_especie'])
+                dicc['nombre_com'] = str(record['nombre_com'])
+                dicc['nombre_cie'] = str(record['nombre_cie'])
+                dicc['espacio_ve'] =str(record['espacio_ve'])
+                dicc['tipo_folla'] =str(record['tipo_folla'])
+                dicc['ubicacion'] =str(record['ubicacion'])
+                dicc['nombre_fam'] =str(record['nombre_fam'])
+                dicc['nombre_gen'] =str(record['nombre_gen'])
+                dicc['origen'] = str(record['origen'])
+                dicc['coord_x'] = float(record['coord_x'])
+                dicc['coord_y'] = float(record['coord_y'])
+                arboles.append(dicc)
+            except ValueError:
+                print(f'Fila {n_fila}: No pude interpretar: {fila}')
+        else:
+            continue
     return arboles
 
-print(leer_parque('../Data/arbolado-en-espacios-verdes.csv'))
+#Determinar si el programa inicio con argumentos:
+#EJemplo de uso:$ python3 arboles.py ../Data/arbolado-en-espacios-verdes.csv
+if len(sys.argv) == 2:
+    nombre_archivo = sys.argv[1]
+else:
+    nombre_archivo = '../Data/arbolado-en-espacios-verdes.csv'
+
+parque = 'GENERAL PAZ'
+pprint(leer_parque(nombre_archivo, parque))
