06fcb89eda0246ee8984729b9b6e210456ea0d81
VMenga <valentin.mengascini@gmail.com>
2021-09-07 15:36:58 -0300

    Finished Propagar.py

--- Clase04/propaga.py
+++ Clase04/propaga.py
@@ -1,5 +1,11 @@
 #propaga.py
 #Por Valentin Mengascini
 
-def propagar(lista):
-    
+lista = []
+def propagar(lis):
+    for count in range(2):
+        for i,f in enumerate(lis):
+            if i - 1 >= 0 and f==0 and lis[i-1]==1:
+                lis[i] = 1
+        lis = lis[::-1]
+    return lis
